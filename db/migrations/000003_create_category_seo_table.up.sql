f793610846119dc189ba58fda004cc68286507d4
insomnia dreams <insomnia.dreams.official@gmail.com>
2020-07-08 14:27:18 +0500

    feat(migrations) add category_seo table and fk

---
+++ db/migrations/000003_create_category_seo_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "category_seo"
+(
+    category_articul CHAR(8) PRIMARY KEY,
+    trans_link       VARCHAR(200),
+    meta_description varchar(500),
+    meta_keywords    varchar(500),
+    description      varchar(2000)
+);
+
